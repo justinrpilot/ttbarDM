cc0465364cb647f44f1ee77ada9a824cfc42c900
decosa <decosa@cern.ch>
2014-07-04 17:34:54 +0200

    clining

--- TopPlusDMAna/python/topplusdmana_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-topPlusDM = cms.EDAnalyzer('TopPlusDMAna',
-   muonBlock = cms.PSet(
-    muon  = cms.InputTag("selectedMuons"),
-    pv    = cms.InputTag("offlinePrimaryVertices"),
-    ptmin = cms.double(5.)
-   ) 
-)
